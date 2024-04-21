1af6aead2889d8d7658ee9c24b1f78c157d0faf4
oasido <asido.ofek@gmail.com>
2024-04-21 09:58:57 +0300

    stop using inbox folder in sb

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -6,12 +6,12 @@ require("obsidian").setup({
     },
   },
 
-  notes_subdir = "0-inbox",
+  -- notes_subdir = "0-inbox",
 
   -- Where to put new notes created from completion. Valid options are
   --  * "current_dir" - put new notes in same directory as the current buffer.
   --  * "notes_subdir" - put new notes in the default notes subdirectory.
-  new_notes_location = "notes_subdir",
+  new_notes_location = "current_dir",
 
   completion = {
     -- Set to false to disable completion.
