54880b091be737057b1a2a9df63c238b59668344
oasido <asido.ofek@gmail.com>
2025-04-15 11:51:12 +0300

    update through script - 2025-04-15 11:51:12

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -139,4 +139,26 @@ return {
       end, { silent = true })
     end,
   },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end,
+      filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
+    },
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('local_detach_ufo', { clear = true }),
+        pattern = opts.filetype_exclude,
+        callback = function()
+          require('ufo').detach()
+        end,
+      })
+
+      vim.opt.foldlevelstart = 99
+      require('ufo').setup(opts)
+    end,
+  },
 }
