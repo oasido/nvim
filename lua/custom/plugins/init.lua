6bcf227cc6c2faf644f8d255eba71b313935908c
oasido <asido.ofek@gmail.com>
2025-04-25 22:06:28 +0300

    update through script - 2025-04-25 22:06:28

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -104,7 +104,11 @@ return {
     'lervag/vimtex',
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.tex_flavor = 'latex'
+      vim.g.vimtex_view_method = 'sioyek'
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.tex_conceal = 'abdmg'
+      vim.opt.conceallevel = 1
     end,
   },
   {
